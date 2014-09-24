4b6d13913228aca13eb0e3b66384a40177427d94
mendoncakeegan <mendoncakeegan@gmail.com>
2014-09-23 18:20:05 -0700

    Some UI changes. Attempted to connect to Corticon

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,34 +4,113 @@
 
 angular.module('myApp.controllers', []).
 controller('AppCtrl', function($scope, $http) {
+    var date = new Date();
+    $scope.currentDate = date.toISOString().substring(0, 10);
     $scope.pane = 0;
+    // This stores all the form data.
     var data = {};
     // This is called each time the screen should be advanced. It stores all current information into data.
     $scope.next = function() {
-        console.log($scope.$$childTail);
-        if($scope.pane == 1){
-            $scope.familySize = 0;
-            if($scope.$$childTail)
-                $scope.familySize = $scope.$$childTail.familySize;
+        switch ($scope.pane) {
+            case 1:
+                $scope.viewCost();
+                break;
+            case 2:
+                $scope.viewGender();
+                break;
+            case 3:
+                data.magazines = [0, 0, 0, 0];
+                if ($scope.magazine1)
+                    data.magazines[0] = 1;
+                if ($scope.magazine2)
+                    data.magazines[1] = 1;
+                if ($scope.magazine3)
+                    data.magazines[2] = 1;
+                if ($scope.magazine4)
+                    data.magazines[3] = 1;
+                break;
         }
-        $scope.pane++;
+        console.log(data);
+        update();
     };
     // This calls the backend to determine what to show next.
     var update = function() {
+        $scope.pane++;
+        return;
         data.pane = $scope.pane;
         $http.post('/api/getInfo', data).
         success(function(dat) {
             console.log(dat);
-            if (dat) {
-                $scope.pane = dat.screen;
-                $scope.sex = dat.sex;
-                $scope.number = dat.number;
-                $scope.estimate = dat.estimate;
-            }
+            if (dat) {}
         });
     }
-    // 
+
     $scope.previous = function() {
         $scope.pane--;
     };
+
+    $scope.viewPlan = function() {
+        return data.plan;
+    };
+
+    $scope.viewCost = function() {
+        if ($scope.pane == 1) {
+            if ($scope.plan == 1) {
+                data.plan = 'Basic';
+            } else if ($scope.plan == 2) {
+                if ($scope.$$childTail.familySize) {
+                    data.familySize = $scope.$$childTail.familySize;
+                }
+                data.plan = 'Economic';
+            } else if ($scope.plan == 3) {
+                data.plan = 'High Deductible';
+            }
+        }
+        data.cost = 0;
+        if (data.plan == 'Economic') {
+            data.cost = data.familySize * 20;
+        }
+        if (data.cost > 100) {
+            data.cost = 100;
+        }
+        if (data.magazines && data.magazines[2]) {
+            data.cost += 10;
+        }
+        return data.cost;
+    }
+    $scope.viewGender = function() {
+        if ($scope.pane == 2) {
+            data.name = $scope.first;
+            data.age = $scope.age;
+            var scope = $scope.$$childTail;
+            if ($scope.gender == 1) {
+                data.gender = 'female';
+                if (scope.pregnant == 2) {
+                    data.pregnant = 'NO';
+                } else {
+                    data.pregnant = 'YES';
+                    scope = scope.$$childTail;
+                    if (scope && scope.duedate) {
+                        data.dueDate = scope.duedate.toISOString().substring(0, 10);
+                    }
+                }
+            } else if ($scope.gender == 2) {
+                data.gender = 'male';
+                if (scope.prostate == 2) {
+                    data.prostate = 'NO';
+                } else {
+                    data.prostate = 'YES';
+                    scope = scope.$$childTail;
+                    if (scope) {
+                        if (scope.history == 2) {
+                            data.history = 'NOT RECENT';
+                        } else if (scope.history == 1) {
+                            data.history = 'RECENT';
+                        }
+                    }
+                }
+            }
+        }
+        return data.gender;
+    }
 });
--- routes/api.js
+++ routes/api.js
@@ -6,31 +6,15 @@ var https = require('https');
 
 exports.getInfo = function(req, result) {
     var data = req.body;
-    /*
-    // This is the simulated response (not using Corticon) on http://health-insurance-22573.onmodulus.net/
-    // Ideally obj.Objects[0].formResponse (line 51) would send an equivalent response to this.
-    var number = [];
-    for (var i = 0; i <= data.familySize; i++)
-        number.push(i);
-    var sex = (data.gender == 'male') ? 'Men' : 'Women';
-    var screen = data.pane + 1;
-    var estimate = 10;
-    result.json({
-        'number': number,
-        'sex': sex,
-        'screen': screen,
-        'estimate': estimate
-    });
-    return;
-    */
     // Using JSON requests based on http://documentation.progress.com/output/ua/Corticon/index.html#page/Corticon/Corticon.0708.html
-    var link = 'http://54.247.33.104:8850/';
-    var hostName = 'http://54.247.33.104:8850/';
+    var link = 'http://54.247.33.104:8850/axis/services/Corticon';
+    var hostName = 'localhost:3000';
     var jsonRequest = JSON.stringify({
         "Objects": [{
             "form": req.body
         }]
     });
+    console.log(req.body)
     var contentLength = jsonRequest.length;
     var request = require('request');
     var querystring = require('querystring');
@@ -41,15 +25,18 @@ exports.getInfo = function(req, result) {
             'connection': 'Keep-Alive',
             'user-agent': 'NodeJS client',
             'host': hostName,
-            'dsName': 'PROCESS NAME HERE'
+            'dsName': 'DisplayDynamicUI'
         },
         url: link,
         body: jsonRequest,
         method: 'POST'
     }, function(err, res, body) {
-        if (res.statusCode < 400) {
+        console.log(res.statusCode);
+        console.log(body);
+        if (res.statusCode <= 400) {
             var obj = JSON.parse(body);
-            var formResponse = obj.Objects[0].formResponse;
+            var formResponse = obj.Objects[0];
+            console.log(formResponse);
             result.write(formResponse);
             result.end();
         }
